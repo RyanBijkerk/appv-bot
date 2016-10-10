4a4e3e55c8c02f62af40364090f746f8227d7ef7
Ryan Bijkerk <ryan@logitblog.com>
2016-10-10 17:43:54 +0200

    Added FromFlow for question regadring App-V (need to figure out what to do next :)

--- AppV-Bot/AppV-Bot/Controllers/MessagesController.cs
+++ AppV-Bot/AppV-Bot/Controllers/MessagesController.cs
@@ -1,40 +1,47 @@
-﻿using System;
-using System.Linq;
-using System.Net;
+﻿using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
+using AppV_Bot.Bots;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
 
 namespace AppV_Bot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+
+        internal static IDialog<AppVPackageBot.AppVCommand> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(AppVPackageBot.AppVCommand.BuildForm));
+        }
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity != null)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                // one of these will have an interface and process it
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRootDialog);
+                        break;
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default: Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                }
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
 
         private Activity HandleSystemMessage(Activity message)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Microsoft Application Virtualization (App-V) Bot
 A Microsoft Bot to talk with your App-V infrastructure
+
+
+# General info:
+https://docs.botframework.com/en-us/csharp/builder/sdkreference/gettingstarted.html
